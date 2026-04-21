d6c02f31430b1b2c39ab098bad27a5b5986f93f5
Maks Pikov <mixelburg@users.noreply.github.com>
2026-04-21 22:07:56 +0000

    fix(controls): prevent save-story bar from overlapping last control

--- code/core/src/controls/components/ControlsPanel.tsx
+++ code/core/src/controls/components/ControlsPanel.tsx
@@ -33,7 +33,8 @@ const clean = (obj: { [key: string]: any }) =>
 const AddonWrapper = styled.div<{ showSaveFromUI: boolean }>(({ showSaveFromUI, theme }) => ({
   height: '100%',
   maxHeight: '100vh',
-  paddingBottom: showSaveFromUI ? 41 : 0,
+  position: 'relative',
+  paddingBottom: showSaveFromUI ? 48 : 0,
   backgroundColor: theme.background.content,
 
   table: {
